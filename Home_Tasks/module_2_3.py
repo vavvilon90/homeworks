c7c9a3a8b16e7baedef29f78cdf9aeb65a9f28bc
vavvilon90 <vavvilonsteam@gmail.com>
2024-10-13 14:20:25 +0500

    13/10

---
+++ Home_Tasks/module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+zero = 0
+while zero < len(my_list):
+    if my_list[zero] > 0:
+        print(my_list[zero])
+        zero += 1
+        continue
+    elif my_list[zero] == 0:
+        zero += 1
+        continue
+    else:
+        break
+
