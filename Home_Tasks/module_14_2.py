2d00f12d114dfabb7904a804b0b1ab9553a3f45b
vavvilon90 <vavvilonsteam@gmail.com>
2024-10-20 12:19:23 +0500

    20/10

---
+++ Home_Tasks/module_14_2.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (f'User{i}', f'example{i}@gmail.com', f'{i}0', '1000'))
+
+cursor.execute('UPDATE Users SET balance = 500 WHERE id % 2 = 1')
+
+cursor.execute('DELETE FROM Users WHERE id % 3 = 1')
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+
+cursor.execute('DELETE FROM Users WHERE id = 6')
+
+cursor.execute('SELECT COUNT(*) FROM Users')
+count_users = cursor.fetchone()[0]
+
+cursor.execute('SELECT SUM(balance) FROM Users')
+sum_balance = cursor.fetchone()[0]
+
+# 1 вариант
+print(sum_balance / count_users)
+
+# 2 вариант
+cursor.execute('SELECT AVG(balance) FROM Users')
+avg_balance = cursor.fetchone()[0]
+print(avg_balance)
+
+columns = cursor.fetchall()
+for column in columns:
+    print(f'Имя: {column[0]} | Почта: {column[1]} | Возраст: {column[2]} | Баланс: {column[3]}')
+
+
+connection.commit()
+connection.close()
